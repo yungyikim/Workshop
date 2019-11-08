3542495e630f55d72d1e609957eccfec91747367
yungyikim <yungyikim@gmail.com>
2019-11-08 20:26:28 +0900

    sitemap 템플릿 수정

--- web/sitemap/generator.py
+++ web/sitemap/generator.py
@@ -72,12 +72,19 @@ class Generator:
 
         t = ''
         for url in self.parsed_urls:
-            t += '<url><loc>{}</loc><lastmod>{}</lastmod><priority>0.80</priority></url>'.format(
+            t += '''<url>
+  <loc>{}</loc>
+  <lastmod>{}</lastmod>
+  <priority>0.80</priority>
+</url>'''.format(
                 url,
                 now
             )
 
-        text = '<?xml version="1.0" encoding="UTF-8"?><urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">{}</urlset>'.format(
+        text = '''<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
+{}
+</urlset>'''.format(
             t
         )
 
